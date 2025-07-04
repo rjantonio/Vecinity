1fe098b00182a02b1ef332950505f17c77389e19
Antonio <rj.antonio2000@gmail.com>
2025-07-04 15:17:42 +0200

    feat: inicio de backend

---
+++ vecinity-backend/Vecinity/src/test/java/com/example/Vecinity/VecinityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Vecinity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VecinityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
