4dd433a83f56db5b23fcb75869d73ecdfe46ef02
Antonio <rj.antonio2000@gmail.com>
2025-07-09 15:09:40 +0200

    feat: ejemplo de conexion con api

--- vecinity-backend/src/main/java/com/example/vecinity/model/EventRegistration.java
+++ vecinity-backend/src/main/java/com/example/vecinity/model/EventRegistration.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "event_registrations", uniqueConstraints = {
-    @UniqueConstraint(columnNames = {"id_usuario", "id_evento"})
+        @UniqueConstraint(columnNames = {"id_usuario", "id_evento"})
 })
 @Data
 @NoArgsConstructor
