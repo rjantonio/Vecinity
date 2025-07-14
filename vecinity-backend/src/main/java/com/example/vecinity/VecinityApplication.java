b9afcfa9870ec37e339d323eb9c5154b730d60c3
Josemi <Josemi@gmail.com>
2025-07-14 16:55:55 +0200

    feat : cambios de diseño en login y register

--- vecinity-backend/src/main/java/com/example/vecinity/VecinityApplication.java
+++ vecinity-backend/src/main/java/com/example/vecinity/VecinityApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VecinityApplication {
+public class 	VecinityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VecinityApplication.class, args);
