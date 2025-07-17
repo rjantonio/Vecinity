51c5e6ae7762238f4a365d52f8d5c2a0f239d3ed
Josemi <Josemi@gmail.com>
2025-07-17 15:58:54 +0200

    fix : cambiar clase main-header duplicada

--- vecinity-frontend/src/components/HomePage.jsx
+++ vecinity-frontend/src/components/HomePage.jsx
@@ -3,18 +3,10 @@ import { useNavigate } from 'react-router-dom';
 import '../css/HomePage.css';
 
 function HomePage() {
-  const navigate = useNavigate();
-
   return (
-      <div className="main-header">
-          <h1>Vecinity</h1>
-          <p>Unidos por una comunidad mejor</p>
-              <button 
-                className="home__cta__button"
-                onClick={() => navigate("/eventos")}>
-                Explorar Eventos
-            </button>
-        </div>
+      <>
+          <p>Sobre nosotros</p>
+      </>
   );
 }
 
